7906623dc096dedeb281a67d4bb048b7b9620552
jeremybond <jeremybond2@hotmail.com>
2013-09-30 15:10:36 +0200

    editing stuff
    added scene's, added random script's, added materials, added tile prefab

---
+++ Assets/Scripts/EnemyBehaviourScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
